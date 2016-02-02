97e1bf973da6bd357da6f491262ad2f923aa8b08
gor <gor@graphite-01>
2016-02-02 13:45:12 +0530

    Parse crash and supervisor logs

--- lib/logstash/filters/greyOrangeCoreCrash.rb
+++ lib/logstash/filters/greyOrangeCoreCrash.rb
@@ -13,19 +13,21 @@ class LogStash::Filters::GreyOrangeCoreCrash < LogStash::Filters::Base
   public
   def filter(event)
         if event["keys"].is_a?(String)
-                field_array = event["keys"].split("\n")
-                $array_length=field_array.length
-                $counter=0
-                custom_event=LogStash::Event:new()
+                custom_event=LogStash::Event.new()
+		custom_event["host"]=event["tags[0]"]
+		custom_event["service"]="server_crash"
+		custom_event["metric"]=0
+		field_array=event["keys"].split("\n")
+		report_type = (field_array[0].split(":"))[0]
+		custom_event["tagfield"]=report_type
+		$array_length=field_array.length
+		$counter=1
                 while $counter < $array_length do
                         field_value=field_array[$counter].split(":")
-                        custom_event["host"]=event["tags[0]"]
-                        custom_event["tagfield"]="key".concat(($counter+1).to_s)
-                        custom_event["service"]="server_crash"
                         custom_event[field_value[0]]=field_value[1]
                         $counter += 1
                 end
-                filter_matched(custome_event)
+                filter_matched(custom_event)
                 yield custom_event
         end
         event.cancel
